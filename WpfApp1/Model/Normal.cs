8fbcf50005c03335d66e8c4425266f8c51b6ac13
Michael <wwwsobaci@mail.ru>
2022-11-16 23:13:30 +0700

    Add normals backend

---
+++ WpfApp1/Model/Normal.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK.Mathematics;
+using System;
+
+namespace WpfApp1.Model;
+
+public class Normal
+{
+   public readonly Vector3 StartPoint;
+   public readonly Vector3 EndPoint;
+   public readonly float XAxisAngle;
+   public readonly float YAxisAngle;
+   public readonly float ZAxisAngle;
+
+
+   public Normal(Vector3 rightHandPoint, Vector3 middlePoint, Vector3 leftHandPoint)
+   {
+      EndPoint = Vector3.Cross(rightHandPoint, leftHandPoint);
+      StartPoint = middlePoint;
+      var dot = Vector3.Dot(new Vector3(1.0f, 0.0f, 0.0f), Vector3.NormalizeFast(StartPoint));
+      XAxisAngle = (float)(Math.Acos(dot) * 180.0 / Math.PI);
+      dot = Vector3.Dot(new Vector3(0.0f, 1.0f, 0.0f), Vector3.NormalizeFast(StartPoint));
+      YAxisAngle = (float)(Math.Acos(dot) * 180.0 / Math.PI);
+      dot = Vector3.Dot(new Vector3(0.0f, 0.0f, 1.0f), Vector3.NormalizeFast(StartPoint));
+      ZAxisAngle = (float)(Math.Acos(dot) * 180.0 / Math.PI);
+   }
+}
---
+++ WpfApp1/Model/PlaneNormals.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using OpenTK.Mathematics;
+
+namespace WpfApp1.Model;
+
+public class PlaneNormals
+{
+    public readonly Normal[] Normals;
+
+    public PlaneNormals(float[] plane)
+    {
+        Normals = new Normal[plane.Length/3];
+        Normals[0] = new Normal
+        (
+            new Vector3(plane[^3], plane[^2], plane[^1]),
+            new Vector3(plane[0], plane[1], plane[2]),
+            new Vector3(plane[3], plane[4], plane[5])
+        );
+        
+        for (int i = 1; i < Normals.Length - 1; i++)
+        {
+            Normals[i] = new Normal
+            (
+                new Vector3(plane[i - 3], plane[i - 2], plane[i - 1]),
+                new Vector3(plane[i], plane[i + 1], plane[i + 2]),
+                new Vector3(plane[i + 3], plane[i + 4], plane[i + 5])
+            );
+        }
+
+        Normals[^1] = new Normal
+        (
+            new Vector3(plane[0], plane[1], plane[2]),
+            new Vector3(plane[^3], plane[^2], plane[^1]),
+            new Vector3(plane[^6], plane[^5], plane[^4])
+        );
+    }
+}
