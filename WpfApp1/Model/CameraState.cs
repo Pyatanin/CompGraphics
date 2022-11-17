28f4ce9d8e642cca0e60a46876d93f8a2f9c34c5
Michael <wwwsobaci@mail.ru>
2022-11-18 00:51:52 +0700

    Add CameraState

---
+++ WpfApp1/Model/CameraState.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfApp1.Model;
+
+public class CameraState
+{
+    public double CurrentCameraSpeed = 0;
+    public double CurrentCameraAngle = 0;
+    public double XAxisCameraPosition = 0;
+    public double YAxisCameraPosition = 10;
+    public double ZAxisCameraPosition = 0;
+    public double HorizontalCameraAngle = 0;
+    public double VerticalCameraAngle = 0;
+}
