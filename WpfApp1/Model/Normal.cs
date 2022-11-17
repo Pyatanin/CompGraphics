46889320a046d48a3ef078a099d489d23be46fca
Michael <wwwsobaci@mail.ru>
2022-11-18 00:56:19 +0700

    Fixed Normals Backend

--- WpfApp1/Model/Normal.cs
+++ WpfApp1/Model/Normal.cs
@@ -1,33 +1,17 @@
-﻿using System;
-using OpenTK.Mathematics;
+﻿using OpenTK.Mathematics;
 
 namespace WpfApp1.Model;
 
 public class Normal
 {
     public readonly Vector3 EndPoint;
-    public readonly Vector3 EndPointNormirovan;
-
     public readonly Vector3 StartPoint;
-    public readonly float XAxisAngle;
-    public readonly float YAxisAngle;
-    public readonly float ZAxisAngle;
-
-
+    
     public Normal(Vector3 rightHandPoint, Vector3 middlePoint, Vector3 leftHandPoint)
     {
-        var CrossVect = Vector3.Cross(rightHandPoint - middlePoint, leftHandPoint - middlePoint);
-        EndPoint = middlePoint + CrossVect;
+        var crossVect = Vector3.Cross(rightHandPoint - middlePoint, leftHandPoint - middlePoint);
         StartPoint = middlePoint;
 
-        EndPointNormirovan = middlePoint + new Vector3(CrossVect.X / CrossVect.Length, CrossVect.Y / CrossVect.Length,
-            CrossVect.Z / CrossVect.Length);
-        var dot = Vector3.Dot(new Vector3(1.0f, 0.0f, 0.0f), Vector3.Normalize(EndPoint - StartPoint));
-        // мейби +90?
-        XAxisAngle = (float)(Math.Acos(dot) * 180.0 / Math.PI);
-        dot = Vector3.Dot(new Vector3(0.0f, 1.0f, 0.0f), Vector3.Normalize(EndPoint - StartPoint));
-        YAxisAngle = (float)(Math.Acos(dot) * 180.0 / Math.PI);
-        dot = Vector3.Dot(new Vector3(0.0f, 0.0f, 1.0f), Vector3.Normalize(EndPoint - StartPoint));
-        ZAxisAngle = (float)(Math.Acos(dot) * 180.0 / Math.PI);
+        EndPoint = middlePoint + Vector3.Normalize(crossVect);
     }
 }
--- WpfApp1/Model/PlaneNormals.cs
+++ WpfApp1/Model/PlaneNormals.cs
@@ -4,80 +4,46 @@ namespace WpfApp1.Model;
 
 public class PlaneNormals
 {
-    public readonly float[] arrayNormals;
     public readonly Normal[] Normals;
 
-
-    public PlaneNormals(float[] plane, bool is3dCoord)
+    public PlaneNormals(float[] plane)
     {
-        if (is3dCoord)
-        {
-            Normals = new Normal[plane.Length / 3];
-            Normals[0] = new Normal
+        Normals = new Normal[plane.Length / 3];
+        Normals[0] = new Normal
+        (
+            new Vector3(plane[^3], plane[^2], plane[^1]),
+            new Vector3(plane[0], plane[1], plane[2]),
+            new Vector3(plane[3], plane[4], plane[5])
+        );
+
+        for (var i = 1; i < Normals.Length - 1; i++)
+        { 
+            Normals[i] = new Normal
             (
-                new Vector3(plane[^3], plane[^2], plane[^1]),
-                new Vector3(plane[0], plane[1], plane[2]),
-                new Vector3(plane[3], plane[4], plane[5])
+                new Vector3(plane[3 * i - 3], plane[3 * i - 2], plane[3 * i - 1]),
+                new Vector3(plane[3 * i], plane[3 * i + 1], plane[3 * i + 2]),
+                new Vector3(plane[3 * i + 3], plane[3 * i + 4], plane[3 * i + 5])
             );
 
-            arrayNormals = new float[plane.Length];
-
-            arrayNormals[0] = Normals[0].EndPointNormirovan.X;
-            arrayNormals[1] = Normals[0].EndPointNormirovan.Y;
-            arrayNormals[2] = Normals[0].EndPointNormirovan.Z;
-
-
-            for (int i = 1; i < Normals.Length - 1; i++)
-            {
-                Normals[i] = new Normal
-                (
-                    new Vector3(plane[3 * i - 3], plane[3 * i - 2], plane[3 * i - 1]),
-                    new Vector3(plane[3 * i], plane[3 * i + 1], plane[3 * i + 2]),
-                    new Vector3(plane[3 * i + 3], plane[3 * i + 4], plane[3 * i + 5])
-                );
-
-                arrayNormals[3 * i] = Normals[i].EndPointNormirovan.X;
-                arrayNormals[3 * i + 1] = Normals[i].EndPointNormirovan.Y;
-                arrayNormals[3 * i + 2] = Normals[i].EndPointNormirovan.Z;
-            }
-
-            Normals[^1] = new Normal
-            (
-                new Vector3(plane[^6], plane[^5], plane[^4]),
-                new Vector3(plane[^3], plane[^2], plane[^1]),
-                new Vector3(plane[0], plane[1], plane[2])
-            );
-
-            arrayNormals[^3] = Normals[^1].EndPointNormirovan.X;
-            arrayNormals[^2] = Normals[^1].EndPointNormirovan.Y;
-            arrayNormals[^1] = Normals[^1].EndPointNormirovan.Z;
         }
-        else
+        Normals[^1] = new Normal
+        (
+            new Vector3(plane[^6], plane[^5], plane[^4]),
+            new Vector3(plane[^3], plane[^2], plane[^1]),
+            new Vector3(plane[0], plane[1], plane[2])
+        );
+    }
+
+    public float[] ToArray()
+    {
+        var normalsArray = new float[Normals.Length * 3];
+        for (var i = 0; i < Normals.Length; i++)
         {
-            Normals = new Normal[plane.Length / 2];
-            Normals[0] = new Normal
-            (
-                new Vector3(plane[^2], plane[^1], 0),
-                new Vector3(plane[0], plane[1], 0),
-                new Vector3(plane[2], plane[3], 0)
-            );
-
-            for (int i = 1; i < Normals.Length - 1; i++)
-            {
-                Normals[i] = new Normal
-                (
-                    new Vector3(plane[2 * i - 2], plane[2 * i - 1], 0),
-                    new Vector3(plane[2 * i], plane[2 * i + 1], 0),
-                    new Vector3(plane[2 * i + 2], plane[2 * i + 3], 0)
-                );
-            }
-
-            Normals[^1] = new Normal
-            (
-                new Vector3(plane[^4], plane[^3], 0),
-                new Vector3(plane[^2], plane[^1], 0),
-                new Vector3(plane[0], plane[1], 0)
-            );
+            normalsArray[i] = Normals[i].EndPoint.X;
+            normalsArray[i + 1] = Normals[i].EndPoint.Y;
+            normalsArray[i + 2] = Normals[i].EndPoint.Z;
         }
+
+        return normalsArray;
     }
 }
