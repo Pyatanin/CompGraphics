8a0ed5d2e3a41032d4cbd13f023176983c03a332
Michael <wwwsobaci@mail.ru>
2022-11-18 00:52:38 +0700

    Add input data for reading from file

---
+++ WpfApp1/Model/InputData.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp1.Model;
+
+public class InputData
+{
+    public float[] Footing { get; init; }
+    public float[] ReplicationVector { get; init; }
+    public float[] RotationVector { get; init; }
+    public float[] ScaleVector { get; init; }
+    public float[] TextureOverlayCoords { get; init; }
+}
