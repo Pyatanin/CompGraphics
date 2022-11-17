7ef635d3e2e585d190ff3916e6d7a445bf212f15
Michael <wwwsobaci@mail.ru>
2022-11-18 00:53:29 +0700

    Add rendering settings

---
+++ WpfApp1/Model/RenderingSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace WpfApp1.Model;
+
+public class RenderingSettings
+{
+    public bool IsObjectVisible = true;
+    public bool IsOrthogonalProjectionOn = false;
+    public bool IsPerspectiveProjectionOn = true;
+    public bool IsCarcaseVisible = false;
+    public bool IsNormalsVisible = false;
+    public bool IsTexturesVisible = false;
+    public bool IsNormalSmoothingEnabled = true;
+    public bool IsNormalSmoothingDisabled = false;
+    public bool IsMouseOnScreen = false;
+    public bool IsMouseMoveForPerspectiveProjection = true;
+}
