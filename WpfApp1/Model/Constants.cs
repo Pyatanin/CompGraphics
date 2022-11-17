478ed64e85a748103ed61d2dfbaf1e5df09da597
Michael <wwwsobaci@mail.ru>
2022-11-18 00:52:12 +0700

    Add constants

---
+++ WpfApp1/Model/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace WpfApp1.Model;
+
+public static class Constants
+{
+    public const double Speed = 0.4;
+    public const double RotationSpeed = 2;
+    public static Point BaseCursorPoint = new(800, 600);
+}
