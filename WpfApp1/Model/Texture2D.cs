8d184b39e0a7df726ad5ac37440893156ac503d0
Michael <wwwsobaci@mail.ru>
2022-11-18 00:55:33 +0700

    Refactor texture2D class

--- WpfApp1/Model/Texture2D.cs
+++ WpfApp1/Model/Texture2D.cs
@@ -3,27 +3,29 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using OpenTK.Graphics.OpenGL;
-using PixelFormat = OpenTK.Graphics.OpenGL.PixelFormat;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
 
 namespace WpfApp1.Model;
 
 public class Texture2D : IDisposable
 {
+    private readonly int _id;
+    public readonly int BufferId;
     public Texture2D(string fileName, float[] coord, TextureWrapMode mod)
     {
-        Bitmap bitmap = new Bitmap(1, 1);
+        var bitmap = new Bitmap(1, 1);
         if (File.Exists(fileName))
         {
             bitmap = (Bitmap)Image.FromFile(fileName);
         }
 
         bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
-        Wight = bitmap.Width;
-        Height = bitmap.Height;
-        BitmapData data = bitmap.LockBits(new Rectangle(0, 0, Wight, Height), ImageLockMode.ReadOnly,
-            System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-        Id = GL.GenTexture();
-        GL.BindTexture(TextureTarget.Texture2D, Id);
+        var width = bitmap.Width;
+        var height = bitmap.Height;
+        var data = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly,
+            PixelFormat.Format32bppArgb);
+        _id = GL.GenTexture();
+        GL.BindTexture(TextureTarget.Texture2D, _id);
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)mod);
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)mod);
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter,
@@ -31,40 +33,32 @@ public class Texture2D : IDisposable
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter,
             (int)TextureMagFilter.Linear);
         GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
-            PixelFormat.Bgra,
+            OpenTK.Graphics.OpenGL.PixelFormat.Bgra,
             PixelType.UnsignedByte, data.Scan0);
         GL.BindTexture(TextureTarget.Texture2D, 0);
         bitmap.UnlockBits(data);
-        Coordinates = coord;
+        var coordinates = coord;
         BufferId = GL.GenBuffer();
         GL.BindBuffer(BufferTarget.ArrayBuffer, BufferId);
-        GL.BufferData(BufferTarget.ArrayBuffer, Coordinates.Length * sizeof(float), Coordinates,
+        GL.BufferData(BufferTarget.ArrayBuffer, coordinates.Length * sizeof(float), coordinates,
             BufferUsageHint.StaticDraw);
         GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
     }
 
-    public int Id { get; private set; }
-    public int Wight { get; private set; }
-
-    public int Height { get; private set; }
-
-    public int BufferId { get; private set; }
-    public float[] Coordinates { get; private set; }
-
     public void Dispose()
     {
         GL.DeleteBuffer(BufferId);
-        GL.DeleteTexture(Id);
+        GL.DeleteTexture(_id);
     }
 
     public void Bind()
     {
-        GL.BindTexture(TextureTarget.Texture2D, Id);
+        GL.BindTexture(TextureTarget.Texture2D, _id);
         GL.BindBuffer(BufferTarget.ArrayBuffer, BufferId);
         GL.TexCoordPointer(2, TexCoordPointerType.Float, 0, IntPtr.Zero);
     }
 
-    public void UnBind()
+    public void Unbind()
     {
         GL.BindTexture(TextureTarget.Texture2D, 0);
         GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
