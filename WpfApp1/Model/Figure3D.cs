d9b5f516d37596bdcc04bdd025bec6319dbbcf8e
Pyatanin <pm93.pyatanin@gmail.com>
2022-11-18 16:35:48 +0700

    Fix plane creation  in figure3d for normals

--- WpfApp1/Model/Figure3D.cs
+++ WpfApp1/Model/Figure3D.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using OpenTK.Graphics.OpenGL;
+
 namespace WpfApp1.Model;
 
 public class Figure3D
@@ -29,7 +30,12 @@ public class Figure3D
 
         BasicPlane = new float[3 * basicPlane.Length / 2];
         ReplicatedPlane = new float[3 * basicPlane.Length / 2];
+
+        Edges = new float[basicPlane.Length / 2][];
+
         var offset = 0;
+
+
         if (replicationVector[^1] < 0)
         {
             offset = 0;
@@ -49,6 +55,44 @@ public class Figure3D
                 ReplicatedPlane[^(i + offset)] = scaleVector[2] * replicationVector[2];
                 offset--;
             }
+
+
+            for (var i = 0; i < basicPlane.Length / 2 - 1; i++)
+            {
+                Edges[i] = new float[12];
+                Edges[i][0] = scaleVector[0] * basicPlane[2 * i];
+                Edges[i][1] = scaleVector[1] * basicPlane[2 * i + 1];
+                Edges[i][2] = 0;
+
+                Edges[i][3] = scaleVector[0] * (basicPlane[2 * i] + replicationVector[0]);
+                Edges[i][4] = scaleVector[1] * (basicPlane[2 * i + 1] + replicationVector[1]);
+                Edges[i][5] = scaleVector[2] * replicationVector[2];
+
+                Edges[i][6] = scaleVector[0] * (basicPlane[2 * i + 2] + replicationVector[0]);
+                Edges[i][7] = scaleVector[1] * (basicPlane[2 * i + 3] + replicationVector[1]);
+                Edges[i][8] = scaleVector[2] * replicationVector[2];
+
+                Edges[i][9] = scaleVector[0] * basicPlane[2 * i + 2];
+                Edges[i][10] = scaleVector[1] * basicPlane[2 * i + 3];
+                Edges[i][11] = 0;
+            }
+
+            Edges[^1] = new float[12];
+            Edges[^1][0] = scaleVector[0] * basicPlane[^2];
+            Edges[^1][1] = scaleVector[1] * basicPlane[^1];
+            Edges[^1][2] = 0;
+
+            Edges[^1][3] = scaleVector[0] * (basicPlane[^2] + replicationVector[0]);
+            Edges[^1][4] = scaleVector[1] * (basicPlane[^1] + replicationVector[1]);
+            Edges[^1][5] = scaleVector[2] * replicationVector[2];
+
+            Edges[^1][6] = scaleVector[0] * (basicPlane[0] + replicationVector[0]);
+            Edges[^1][7] = scaleVector[1] * (basicPlane[1] + replicationVector[1]);
+            Edges[^1][8] = scaleVector[2] * replicationVector[2];
+
+            Edges[^1][9] = scaleVector[0] * basicPlane[0];
+            Edges[^1][10] = scaleVector[1] * basicPlane[1];
+            Edges[^1][11] = 0;
         }
         else
         {
@@ -69,45 +113,45 @@ public class Figure3D
                 ReplicatedPlane[i + offset + 2] = scaleVector[2] * replicationVector[2];
                 offset++;
             }
+
+            for (var i = 0; i < basicPlane.Length / 2 - 1; i++)
+            {
+                Edges[i] = new float[12];
+                Edges[i][0] = scaleVector[0] * basicPlane[2 * i];
+                Edges[i][1] = scaleVector[1] * basicPlane[2 * i + 1];
+                Edges[i][2] = 0;
+
+                Edges[i][3] = scaleVector[0] * basicPlane[2 * i + 2];
+                Edges[i][4] = scaleVector[1] * basicPlane[2 * i + 3];
+                Edges[i][5] = 0;
+
+                Edges[i][6] = scaleVector[0] * (basicPlane[2 * i + 2] + replicationVector[0]);
+                Edges[i][7] = scaleVector[1] * (basicPlane[2 * i + 3] + replicationVector[1]);
+                Edges[i][8] = scaleVector[2] * replicationVector[2];
+
+                Edges[i][9] = scaleVector[0] * (basicPlane[2 * i] + replicationVector[0]);
+                Edges[i][10] = scaleVector[1] * (basicPlane[2 * i + 1] + replicationVector[1]);
+                Edges[i][11] = scaleVector[2] * replicationVector[2];
+            }
+
+            Edges[^1] = new float[12];
+            Edges[^1][0] = scaleVector[0] * basicPlane[^2];
+            Edges[^1][1] = scaleVector[1] * basicPlane[^1];
+            Edges[^1][2] = 0;
+
+            Edges[^1][3] = scaleVector[0] * basicPlane[0];
+            Edges[^1][4] = scaleVector[1] * basicPlane[1];
+            Edges[^1][5] = 0;
+
+            Edges[^1][6] = scaleVector[0] * (basicPlane[0] + replicationVector[0]);
+            Edges[^1][7] = scaleVector[1] * (basicPlane[1] + replicationVector[1]);
+            Edges[^1][8] = scaleVector[2] * replicationVector[2];
+
+            Edges[^1][9] = scaleVector[0] * (basicPlane[^2] + replicationVector[0]);
+            Edges[^1][10] = scaleVector[1] * (basicPlane[^1] + replicationVector[1]);
+            Edges[^1][11] = scaleVector[2] * replicationVector[2];
         }
 
-        Edges = new float[basicPlane.Length / 2][];
-        for (var i = 0; i < basicPlane.Length / 2 - 1; i++)
-        {
-            Edges[i] = new float[12];
-            Edges[i][0] = scaleVector[0] * basicPlane[2 * i];
-            Edges[i][1] = scaleVector[1] * basicPlane[2 * i + 1];
-            Edges[i][2] = 0;
-
-            Edges[i][3] = scaleVector[0] * basicPlane[2 * i + 2];
-            Edges[i][4] = scaleVector[1] * basicPlane[2 * i + 3];
-            Edges[i][5] = 0;
-
-            Edges[i][6] = scaleVector[0] * (basicPlane[2 * i + 2] + replicationVector[0]);
-            Edges[i][7] = scaleVector[1] * (basicPlane[2 * i + 3] + replicationVector[1]);
-            Edges[i][8] = scaleVector[2] * replicationVector[2];
-
-            Edges[i][9] = scaleVector[0] * (basicPlane[2 * i] + replicationVector[0]);
-            Edges[i][10] = scaleVector[1] * (basicPlane[2 * i + 1] + replicationVector[1]);
-            Edges[i][11] = scaleVector[2] * replicationVector[2];
-        }
-
-        Edges[^1] = new float[12];
-        Edges[^1][0] = scaleVector[0] * basicPlane[^2];
-        Edges[^1][1] = scaleVector[1] * basicPlane[^1];
-        Edges[^1][2] = 0;
-
-        Edges[^1][3] = scaleVector[0] * basicPlane[0];
-        Edges[^1][4] = scaleVector[1] * basicPlane[1];
-        Edges[^1][5] = 0;
-
-        Edges[^1][6] = scaleVector[0] * (basicPlane[0] + replicationVector[0]);
-        Edges[^1][7] = scaleVector[1] * (basicPlane[1] + replicationVector[1]);
-        Edges[^1][8] = scaleVector[2] * replicationVector[2];
-
-        Edges[^1][9] = scaleVector[0] * (basicPlane[^2] + replicationVector[0]);
-        Edges[^1][10] = scaleVector[1] * (basicPlane[^1] + replicationVector[1]);
-        Edges[^1][11] = scaleVector[2] * replicationVector[2];
 
         BasicPlaneNormals = new PlaneNormals(BasicPlane);
         ReplicatedPlaneNormals = new PlaneNormals(ReplicatedPlane);
@@ -119,7 +163,8 @@ public class Figure3D
         }
 
         BasicPlaneTexture = new Texture2D(basicPlaneTexture, textureOverlayCoordinates, TextureWrapMode.Clamp);
-        ReplicatedPlaneTexture = new Texture2D(replicatedPlaneTexture, textureOverlayCoordinates, TextureWrapMode.Clamp);
+        ReplicatedPlaneTexture =
+            new Texture2D(replicatedPlaneTexture, textureOverlayCoordinates, TextureWrapMode.Clamp);
         EdgesTexture = new Texture2D(edgeTexture, textureOverlayCoordinates, TextureWrapMode.Clamp);
     }
 }
