288d78cc0b480659247a187e69408147ae48733b
Michael <wwwsobaci@mail.ru>
2022-11-18 15:41:44 +0700

    Add sun backend

---
+++ WpfApp1/Model/Sun.cs
@@ -0,0 +1,17 @@
+﻿namespace WpfApp1.Model;
+using OpenTK.Graphics.OpenGL;
+
+public class Sun
+{
+    public float SunPosition = 0.0f;
+    public readonly float[] SunCoordinates;
+    public readonly float[] TextureOverlayCoordinates;
+    public readonly Texture2D SunTexture;
+
+    public Sun(float[] sunCoordinates, string sunTexture, float[] textureOverlayCoordinates)
+    {
+        SunCoordinates = sunCoordinates;
+        TextureOverlayCoordinates = textureOverlayCoordinates;
+        SunTexture = new Texture2D(sunTexture, textureOverlayCoordinates, TextureWrapMode.Clamp);
+    }
+}
